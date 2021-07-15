27015d33629a5a1cef1ffab63a37aba1432f9a84
akemi <79666957+mimiyama@users.noreply.github.com>
2021-07-15 12:27:50 -0300

    a

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key: 'Cq3ynhvqKrLsVClZN4MlJPSnf',
-    consumer_secret: '6QBd370boyQsyVURyAXHeiGTTIQ7K11WZVS4NSnFSI851WW8ww',
-    access_token: '1384177622737821697-r34PaDj3JWnYt2QWS1fiefJLNkg650',
-    access_token_secret: 'kXPxV95YN0201cJqoN4dPq2sQE2rkBSuESo7zYnZzxU1Y',
-    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAK3XQAEAAAAAO%2B9DSsqhLZZqQDH537fNG3SSU2w%3DvClSL0LtEqIcDZ8RtUQCDaUwGkUM13R1Q143qq4ZlamNs8cfym',
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: '',
+    bearer_token: '',
 }
