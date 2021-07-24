4ad947abb3567f0e1ef63786afb2263c5c2edcce
lígia <79666957+mimiyama@users.noreply.github.com>
2021-07-24 16:29:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const Bot = new Twit(config);
 
 console.log('Este bot está rodando...');
 
-// array para armazenar tweets já respondidos
 var tweetsRespondidos = [''];
-
-// array para armazenar as respostas do bot
 var respostasTweet = [''];
 
 function encontrarTweets(){
-    // filtrando  
+ 
     Bot.get("search/tweets", {q: 'oi @nomebot' }, (err, data) => {
         if (err) {
             console.log(err.message);
@@ -19,15 +16,11 @@ function encontrarTweets(){
             console.log('achou!');
             var tweetId = data.statuses[0].id_str;
             var username = data.statuses[0].user.screen_name;
-            
-            // selecionar aleatoriamente uma resposta para o tweet armazenada no respostasTweet[]
             var mostrarRespostas = respostasTweet[Math.floor((Math.random() * respostasTweet.length))];
   
             if (tweetsRespondidos.includes(tweetId)){
                 console.log('Esse tweet já foi respondido!');
             } else {
-                
-                // mandando a reply
                 Bot.post('statuses/update', Bot.post('statuses/update', { in_reply_to_status_id: tweetId, status: `@${username} ${mostrarRespostas}` }, function(err){
                     console.log('Respondeu ao: ', tweetId);
                     if (err) {
@@ -36,7 +29,6 @@ function encontrarTweets(){
                 }))
             }
           
-            // armazenando o tweet respondido para o array de tweetsRespondidos[]
             tweetsRespondidos.push(tweetId);
         }
     })
